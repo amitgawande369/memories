99262af2d47e9bb9cc3cedb1fd38e7d9b169b06f
amitgawande369 <amitgawande002@gmail.com>
2023-12-05 16:07:38 +0530

    commit1

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+// app.use(bodyParser.json({limit: "30mb", extended: true}))
+// app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(express.json({limit: "30mb", extended: true}))
+app.use(express.urlencoded({limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL = "mongodb+srv://amitgawande002:Memories1234@cluster0.5w2wkea.mongodb.net/"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log("server running on port: ", PORT)))
+.catch((error) => console.log(error))
+
+// mongoose.set('useFindAndModify', false)
