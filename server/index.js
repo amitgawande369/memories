c1400e6435963879ca20cd7a7dc65c5a2bb60aaa
amitgawande369 <amitgawande002@gmail.com>
2023-12-10 11:28:52 +0530

    commit3

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
 const app = express();
 
 // app.use(bodyParser.json({limit: "30mb", extended: true}))
@@ -10,8 +11,8 @@ const app = express();
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
+app.use('/posts', postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://amitgawande002:Memories1234@cluster0.5w2wkea.mongodb.net/"
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log("server running on port: ", PORT)))
